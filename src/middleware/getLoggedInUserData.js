de3f6b94d2d2660d1d81e25c18efbf5e39f4c2c8
Ahmad Alawneh <aalawneh19@gmail.com>
2023-11-14 23:32:32 +0300

    Initial work

--- src/middleware/getLoggedInUserData.js
+++ src/middleware/getLoggedInUserData.js
@@ -1,5 +1,17 @@
+const requestService = require('../requestService');
+const { verifyUserToken } = require('../shared/tokenService');
+
 const getLoggedInUserData = async (req, res, next) => {
-	return next();
+	return await requestService.handleRequest(req, res, next, async () => {
+		if (!req.header('Authorization')) {
+			return res.status(401).send({ error: true, code: 401, message: 'Authentication required' });
+		}
+		const token = req.header('Authorization').replace('Bearer ', '');
+		const decodedToken = verifyUserToken(token);
+		req.user = decodedToken;
+
+		return next();
+	});
 };
 
 module.exports = getLoggedInUserData;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
+const requestService = require('./requestService');
 require('./database/startConnection').connect().catch(err => console.log(err));
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
+
+app.use(requestService.logRequest);
 
 app.get('/', async (req, res) => {
 	res.send('Hello World!');
@@ -13,6 +16,9 @@ app.get('/', async (req, res) => {
 
 app.use('/users', userRouter);
 
+app.use(requestService.handleGenericError);
+
+const port = 3000;
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
